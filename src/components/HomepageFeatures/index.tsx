b7550fbc679e1738e89ff018bf344f1093488a42
Eric Do <ericdo.617@gmail.com>
2022-04-30 18:31:17 -0500

    Fix alignment of reviews

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -54,7 +54,7 @@ function Feature({title, Svg, description}: FeatureItem) {
 
 export default function HomepageFeatures(): JSX.Element {
   return (
-    <section className={styles.features}>
+    <section className={clsx(styles.features)}>
 
       <div className="container">
         <div className={styles.sectionTitle}>
--- src/components/HomepageReviews/index.tsx
+++ src/components/HomepageReviews/index.tsx
@@ -7,6 +7,7 @@ type ReviewItem = {
   linkedIn?: string;
   avatar: string;
   employer?: string;
+  role?: string;
   review: string;
 }
 
@@ -16,43 +17,63 @@ const ReviewList: ReviewItem[] = [
     linkedIn: 'ericdo1',
     avatar: 'https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg',
     employer: 'Galvanize',
-    review: 'Eric delivers such an awesome service'
+    role: 'Instructor',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc, eget aliquam velit molestie vel. Donec ornare, ipsum in sagittis bibendum, tortor nulla dictum odio, nec efficitur eros eros vitae metus. Praesent non facilisis tellus.'
   },
   {
     name: 'Eric',
     linkedIn: 'ericdo1',
     avatar: 'https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg',
     employer: 'Galvanize',
-    review: 'Eric delivers such an awesome service'
+    role: 'Instructor',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc, eget aliquam velit molestie vel. Donec ornare, ipsum in sagittis bibendum, tortor nulla dictum odio, nec efficitur eros eros vitae metus. Praesent non facilisis tellus.'
   },
   {
     name: 'Eric',
     linkedIn: 'ericdo1',
     avatar: 'https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg',
     employer: 'Galvanize',
-    review: 'Eric delivers such an awesome service'
+    role: 'Instructor',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc, eget aliquam velit molestie vel. Donec ornare, ipsum in sagittis bibendum, tortor nulla dictum odio, nec efficitur eros eros vitae metus. Praesent non facilisis tellus.'
   },
   {
     name: 'Eric',
     linkedIn: 'ericdo1',
     avatar: 'https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg',
     employer: 'Galvanize',
-    review: 'Eric delivers such an awesome service'
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc, eget aliquam velit molestie vel. Donec ornare, ipsum in sagittis bibendum, tortor nulla dictum odio, nec efficitur eros eros vitae metus. Praesent non facilisis tellus.'
+  },
+  {
+    name: 'Eric',
+    linkedIn: 'ericdo1',
+    avatar: 'https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg',
+    employer: 'Galvanize',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc, eget aliquam velit molestie vel. Donec ornare, ipsum in sagittis bibendum, tortor nulla dictum odio, nec efficitur eros eros vitae metus. Praesent non facilisis tellus.'
   },
 ]
 
-const Review = ({ name, linkedIn, avatar, employer, review }: ReviewItem) => {
+const Review = ({
+  name,
+  linkedIn,
+  avatar,
+  employer,
+  review,
+  role
+}: ReviewItem) => {
   return (
     <div className={clsx('col col--4')}>
-      <div className="card">
-        <div className="padding-horiz--md">
+      <div className={clsx("card", "margin-top--lg")}>
+        <div className="card__header">
           <div className="avatar">
             <img
               className="avatar__photo-link avatar__photo avatar__photo--lg"
               alt={`${name} profile image`}
               src={avatar} />
-            <div className="avatar__intro">
+            <div className={clsx("avatar__intro", styles.cardContent)}>
               <div className="avatar__name">{name}</div>
+              <small className="avatar__subtitle">
+                {`${role}, ${employer}`}
+              </small>
               <small className="avatar__subtitle">
                 {review}
               </small>
