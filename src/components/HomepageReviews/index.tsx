6b8891cf7cc4e46bcf0d2190edd9fe5b8b907ac4
Eric Do <ericdo.617@gmail.com>
2022-05-01 00:04:37 -0500

    Put reviews into columns

--- src/components/HomepageReviews/index.tsx
+++ src/components/HomepageReviews/index.tsx
@@ -11,57 +11,6 @@ type ReviewItem = {
   review: string;
 };
 
-const ReviewList: ReviewItem[] = [
-  {
-    name: "Eric",
-    linkedIn: "ericdo1",
-    avatar:
-      "https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg",
-    employer: "Galvanize",
-    role: "Instructor",
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc, eget aliquam velit molestie vel. Donec ornare, ipsum in sagittis bibendum, tortor nulla dictum odio, nec efficitur eros eros vitae metus. Praesent non facilisis tellus.",
-  },
-  {
-    name: "Eric",
-    linkedIn: "ericdo1",
-    avatar:
-      "https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg",
-    employer: "Galvanize",
-    role: "Instructor",
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc, eget aliquam velit molestie vel. Donec ornare.",
-  },
-  {
-    name: "Eric",
-    linkedIn: "ericdo1",
-    avatar:
-      "https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg",
-    employer: "Galvanize",
-    role: "Instructor",
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc",
-  },
-  {
-    name: "Eric",
-    linkedIn: "ericdo1",
-    avatar:
-      "https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg",
-    employer: "Galvanize",
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc, eget aliquam velit molestie vel. Donec ornare, ipsum in sagittis bibendum, tortor nulla dictum odio, nec efficitur eros eros vitae metus. Praesent non facilisis tellus.",
-  },
-  {
-    name: "Eric",
-    linkedIn: "ericdo1",
-    avatar:
-      "https://res.cloudinary.com/dngetymia/image/upload/v1648187630/business-app/profile_eibjrm.jpg",
-    employer: "Galvanize",
-    review:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue tellus lectus, sit amet iaculis neque efficitur et. Aliquam pulvinar lobortis nunc, eget aliquam velit molestie vel. Donec ornare, ipsum in sagittis bibendum, tortor nulla dictum odio, nec efficitur eros eros vitae metus. Praesent non facilisis tellus.",
-  },
-];
-
 const ReviewListCol1: ReviewItem[] = [
   {
     name: "Eric",
@@ -170,27 +119,37 @@ const Review = ({
   role,
 }: ReviewItem) => {
   return (
-    <div className={clsx("col col--4")}>
-      <div className={clsx("card", "margin-top--lg", styles.cardContent)}>
-        <div className="card__header">
-          <div className="avatar">
-            <img
-              className="avatar__photo-link avatar__photo avatar__photo--lg"
-              alt={`${name} profile image`}
-              src={avatar}
-            />
-            <div className="avatar__intro">
-              <div className="avatar__name">{name}</div>
-              <small className="avatar__subtitle">
-                {`${role}, ${employer}`}
-              </small>
-            </div>
+    <div className={clsx("card", "margin-top--lg", styles.cardContent)}>
+      <div className="card__header">
+        <div className="avatar">
+          <img
+            className="avatar__photo-link avatar__photo avatar__photo--lg"
+            alt={`${name} profile image`}
+            src={avatar}
+          />
+          <div className="avatar__intro">
+            <div className="avatar__name">{name}</div>
+            <small className="avatar__subtitle">
+              {`${role}, ${employer}`}
+            </small>
           </div>
         </div>
-        <div className="card__body">
-          <small className="avatar__subtitle">{review}</small>
-        </div>
       </div>
+      <div className="card__body">
+        <small className="avatar__subtitle">{review}</small>
+      </div>
+    </div>
+  );
+};
+
+const ReviewColumn = ({reviews}: { reviews: ReviewItem[]}) => {
+  return (
+    <div className={clsx("col col--4")}>
+      {
+        reviews.map((props, idx) => (
+          <Review key={idx} {...props} />
+        ))
+      }
     </div>
   );
 };
@@ -206,9 +165,9 @@ const HomepageReviews = (): JSX.Element => {
           We've personally taught and guided hundreds of students to successful careers as software engineers.
         </p>
         <div className="row">
-          {ReviewList.map((props, idx) => (
-            <Review key={idx} {...props} />
-          ))}
+          <ReviewColumn reviews={ReviewListCol1} />
+          <ReviewColumn reviews={ReviewListCol2} />
+          <ReviewColumn reviews={ReviewListCol3} />
         </div>
       </div>
     </section>
